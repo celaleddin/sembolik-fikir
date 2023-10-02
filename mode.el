56df470899700f480ee2b30d1eac295da3ab2232
Celaleddin Hidayetoglu <celaleddin.hidayetoglu@gmail.com>
2023-10-03 01:16:27 +0400

    Introduce simple indentation for Emacs

--- mode.el
+++ mode.el
@@ -14,7 +14,28 @@
     ("\".*\"" 0 font-lock-string-face)
     ("[0-9]+" 0 font-lock-constant-face))
   '("\\.sf$")
-  nil
+  (list
+   (lambda ()
+     (setq-local tab-width 2)
+     (setq-local indent-tabs-mode nil)
+     (setq-local indent-line-function 'simple-indent-line)))
   "A mode for sembolik-fikir files")
 
-(add-hook 'sf-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'sf-mode-hook (lambda () (rainbow-delimiters-mode 1)))
+(add-hook 'sf-mode-hook (lambda () (smartparens-mode 1)))
+
+;; Below indentation code is based on:
+;; https://gist.github.com/eddieh/33aaa48625001992d8584bfdd5008501
+(defun simple-indent-line ()
+  (interactive)
+  (let ((savep (> (current-column) (current-indentation)))
+        (indent (condition-case nil (max (simple-calculate-indentation) 0)
+                  (error 0))))
+    (if savep
+        (save-excursion (indent-line-to indent))
+      (indent-line-to indent))))
+
+(defun simple-calculate-indentation ()
+  "Return the column to which the current line should be indented."
+  (* tab-width (min (car (syntax-ppss (line-beginning-position)))
+                    (car (syntax-ppss (line-end-position))))))
